40deb695f9bd56f08ccb97ed938fe0cb7d663d39
jeffrey <jeffrey@steamboat>
2016-12-04 20:32:08 +0100

    [MOD] ToggleController with permission

--- controllers/ToggleController.php
+++ controllers/ToggleController.php
@@ -7,7 +7,9 @@ namespace humanized\maintenance\controllers;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 use yii\web\Controller;
+use yii\filters\AccessControl;
 
 class ToggleController extends Controller
 {
@@ -23,6 +25,15 @@ class ToggleController extends Controller
             'enable' => 'humanized\maintenance\actions\EnableAction',
             'disable' => 'humanized\maintenance\actions\DisableAction',
         ];
+    }
+
+    public function beforeAction($action)
+    {
+        $permission = isset($this->module->params['togglePermission']) ? $this->module->params['togglePermission'] : null;
+        if (isset($permission) && !Yii::$app->user->can($permission)) {
+            return false;
+        }
+        return parent::beforeAction($action);
     }
 
 }
--- widgets/ToggleSwitch.php
+++ widgets/ToggleSwitch.php
@@ -1,8 +1,34 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace humanized\maintenance\widgets;
 
+use yii\base\Widget;
+use yii\helpers\Html;
+use humanized\maintenance\models\Maintenance;
+
+class ToggleSwitch extends Widget
+{
+
+    public $label;
+    public $encodeLabel = true;
+    public $options = [];
+    public $url = ['/maintenance/toggle'];
+    protected $isEnabled = false;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->isEnabled = Maintenance::isEnabled();
+        if (!isset($this->label)) {
+            $prefix = $this->isEnabled ? 'Disable' : 'Enable';
+            $this->label = $prefix . ' ' . 'Maintenance Mode';
+        }
+    }
+
+    public function run()
+    {
+        return Html::a($this->encodeLabel ? Html::encode($this->label) : $this->label, $this->url, $this->options);
+    }
+
+}
